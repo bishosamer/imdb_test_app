06e1eb55e20d9a1a10e8f93870880dc23a0efb31
beshoyishak <114907141+beshoyishak@users.noreply.github.com>
2023-07-01 01:28:16 +0300

    fixed small bug with parsing popularity value

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -37,7 +37,7 @@ class Movie {
         originalLanguage: json['original_language'],
         originalTitle: json['original_title'],
         overview: json['overview'],
-        popularity: json['popularity'],
+        popularity: double.parse(json['popularity'].toString()),
         posterPath: json['poster_path'],
         releaseDate: json['release_date'],
         title: json['title'],
